37fecbe7c9305982deffa86c726da5601e602319
colman423 <colman0423@gmail.com>
2019-08-16 01:03:43 +0800

    master移除source map、加上get timestamp

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/*', function (req, res) {
-    console.log(req.path)
+    console.log(new Date().toUTCString(), req.path||"")
     // console.log(req.route)
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
